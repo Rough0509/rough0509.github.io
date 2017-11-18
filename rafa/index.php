7c5bf21df16812aed2509b72e371237352633a65
Rafa <rafik_9@mail.ru>
2017-11-18 03:11:21 +0300

    dick commit

---
+++ rafa/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>сайт на php</title>
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<div id="page-wrap">
+					
+
+	<header>
+		<a href="index.php>"><div class="logo">RAFA.com</div></a>
+		<div class="menu"><a href="aboutus.php">О компании</a>    |    <a href="prod.php">Продукты</a>    |   <a href="forum.php"> Форум</a></div>
+		<div class="sign"><a href="login.php">Вход</a> | <a href="reg.php">Регистрация</a></div>
+
+		<div class="container">
+			<div class="content">
+				<img src="img/hazard.jpg" style="width: 100%; height: 690px;" alt="">
+				<article class="article"></article>
+			</div>
+			<div class="sidebar">
+				<img src="img/sea.jpg" style="width: 100%; height: 690px;" alt="">
+				<article class="article2"></article>
+			</div>
+		</div>
+</div>
+		<footer>
+<span class="left">All Rights Reserved &copy 2017</span>
+<span class="center">Политика конфедециальности </span>
+<span class="centered">Наши партнеры </span>
+</footer>
+
+	</header>
+</body>
+</html>
